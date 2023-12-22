abf9291803326432e18a44040ede1eb3216acfaa
Abhishek Yadav <abhishek_yadav_work@outlook.com>
2023-12-23 04:18:14 +0530

    Registered User

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS.ORIGIN,
+    origin: process.env.CORS_ORIGIN,
     credential:true
 }))
 
@@ -13,6 +13,14 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+
+//routes
+import userRouter from './routes/user.routes.js';
+
+app.use("/api/v1/users", userRouter)
+
 
 
 export{app}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 //require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'/env'
+    path:'./env'
 })
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000, ()=>{
-        console.log(`Server is running at port ${process.env.PORT}`);
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
-.catch(()=>{
-    console.log("Mongo db connection failed!!!", err);
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
+
 
 
 
