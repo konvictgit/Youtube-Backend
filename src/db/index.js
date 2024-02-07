c0fa19bf049fa180d375f589313a4aeb64f9bf8f
Abhishek Yadav <abhishek_yadav_work@outlook.com>
2024-02-07 16:24:50 +0530

    bug

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,8 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async()=>{
     try {
-      const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+      const connectionInstance = await mongoose.connect("mongodb+srv://AbhishekYadav:WgZ4phmb0Ys0G6EC@cluster0.nq3ls62.mongodb.net/"+DB_NAME)
+      console.log(process.env.MONGODB_URI);
       console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`); 
     } catch (error) {
         console.log("MONGODB connection error",error);
--- src/models/video.models.js
+++ src/models/video.models.js
@@ -41,6 +41,5 @@ const videoSchema = new Schema(
         timestamps:true
     }
 )
-
 videoSchema.plugin(mongooseAggregatePaginate)
 export const Video = mongoose.model("Video",videoSchema)
